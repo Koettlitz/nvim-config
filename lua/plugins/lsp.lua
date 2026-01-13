47ce98e71a15531272c1c439d57b8f9993dc7396
david <david_koettlitz@proton.me>
2026-01-13 09:24:38 +0100

    Auto fmt on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,14 @@
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    for _, client in ipairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      if client.supports_method("textDocument/formatting") then
+        vim.lsp.buf.format({ async = false })
+        return
+      end
+    end
+  end,
+})
+
 return {
   {
     "neovim/nvim-lspconfig",
