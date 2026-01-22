d1a17f360139ec4192a27d40cf4117dffbb88533
david <david_koettlitz@proton.me>
2026-01-22 12:26:39 +0100

    More surround maps

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -3,6 +3,21 @@ return {
     version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup()
+        require("nvim-surround").setup({
+            surrounds = {
+                ["O"] = {
+                    add = { "Option<", ">" },
+                },
+                ["s"] = {
+                    add = { "Some(", ")" },
+                },
+                ["o"] = {
+                    add = { "Ok(", ")" },
+                },
+                ["e"] = {
+                    add = { "Err(", ")" },
+                },
+            }
+        })
     end
 }
